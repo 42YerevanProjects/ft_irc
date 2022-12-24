4939d6cc4ae5becc306d26b46f5e112847d5ed06
Sargis-Hovsepyan <sargishovsepyan@gmail.com.com>
2022-12-24 17:13:35 +0400

    [ADD] Added 42 header

--- includes/response.hpp
+++ includes/response.hpp
@@ -25,34 +25,34 @@
 #define ERR_NEEDMOREPARAMS(source, command)             "461 " + source + " " + command + " :Not enough parameters"
 
 #define ERR_TOOMANYCHANNELS(source, channel)            "405 " + source + " " + channel + " :You have joined too many channels"
-#define ERR_NOTONCHANNEL(source, channel)				"442 " + source + " " + channel + " :You're not on that channel"
-#define ERR_NOSUCHCHANNEL(source, channel)				"403 " + source + " " + channel + " :No such channel"
-#define ERR_BADCHANNELKEY(source, channel)				"475 " + source + " " + channel + " :Cannot join channel (+k)"
-#define ERR_CHANNELISFULL(source, channel)				"471 " + source + " " + channel + " :Cannot join channel (+l)"
-#define ERR_CANNOTSENDTOCHAN(source, channel)			"404 " + source + " " + channel + " :Cannot send to channel"
-#define ERR_CHANOPRIVSNEEDED(source, channel)			"482 " + source + " " + channel + " :You're not channel operator"
+#define ERR_NOTONCHANNEL(source, channel)               "442 " + source + " " + channel + " :You're not on that channel"
+#define ERR_NOSUCHCHANNEL(source, channel)              "403 " + source + " " + channel + " :No such channel"
+#define ERR_BADCHANNELKEY(source, channel)              "475 " + source + " " + channel + " :Cannot join channel (+k)"
+#define ERR_CHANNELISFULL(source, channel)              "471 " + source + " " + channel + " :Cannot join channel (+l)"
+#define ERR_CANNOTSENDTOCHAN(source, channel)           "404 " + source + " " + channel + " :Cannot send to channel"
+#define ERR_CHANOPRIVSNEEDED(source, channel)           "482 " + source + " " + channel + " :You're not channel operator"
 
-#define ERR_NOSUCHNICK(source, nickname)				"401 " + source + " " + nickname + " :No such nick/channel"
-#define ERR_USERNOTINCHANNEL(source, nickname, channel)	"441 " + source + " " + nickname + " " + channel + " :They aren't on that channel"
+#define ERR_NOSUCHNICK(source, nickname)                "401 " + source + " " + nickname + " :No such nick/channel"
+#define ERR_USERNOTINCHANNEL(source, nickname, channel) "441 " + source + " " + nickname + " " + channel + " :They aren't on that channel"
 
 
 /* Numeric Responses */
 
-#define RPL_WELCOME(source)						        "001 " + source + " :Welcome " + source + " to the ft_irc network"
-#define RPL_NAMREPLY(source, channel, users)	        "353 " + source + " = " + channel + " :" + users
-#define RPL_ENDOFNAMES(source, channel)			        "366 " + source + " " + channel + " :End of /NAMES list."
+#define RPL_WELCOME(source)                             "001 " + source + " :Welcome " + source + " to the ft_irc network"
+#define RPL_NAMREPLY(source, channel, users)            "353 " + source + " = " + channel + " :" + users
+#define RPL_ENDOFNAMES(source, channel)                 "366 " + source + " " + channel + " :End of /NAMES list."
 
 
 /* Command Responses */
 
-#define RPL_JOIN(source, channel)					    ":" + source + " JOIN :" + channel
-#define RPL_PART(source, channel)					    ":" + source + " PART :" + channel
-#define RPL_PING(source, token)						    ":" + source + " PONG :" + token
-#define RPL_PRIVMSG(source, target, message)	    	":" + source + " PRIVMSG " + target + " :" + message
-#define RPL_NOTICE(source, target, message)		    	":" + source + " NOTICE " + target + " :" + message
-#define RPL_QUIT(source, message)					    ":" + source + " QUIT :Quit: " + message
-#define RPL_KICK(source, channel, target, reason)	    ":" + source + " KICK " + channel + " " + target + " :" + reason
-#define RPL_MODE(source, channel, modes, args)		    ":" + source + " MODE " + channel + " " + modes + " " + args
+#define RPL_JOIN(source, channel)                       ":" + source + " JOIN :" + channel
+#define RPL_PART(source, channel)                       ":" + source + " PART :" + channel
+#define RPL_PING(source, token)                         ":" + source + " PONG :" + token
+#define RPL_PRIVMSG(source, target, message)            ":" + source + " PRIVMSG " + target + " :" + message
+#define RPL_NOTICE(source, target, message)             ":" + source + " NOTICE " + target + " :" + message
+#define RPL_QUIT(source, message)                       ":" + source + " QUIT :Quit: " + message
+#define RPL_KICK(source, channel, target, reason)       ":" + source + " KICK " + channel + " " + target + " :" + reason
+#define RPL_MODE(source, channel, modes, args)          ":" + source + " MODE " + channel + " " + modes + " " + args
 
 #endif
 
