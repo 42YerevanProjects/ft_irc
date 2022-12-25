9abd5557baa734d283aee4e41db558fda0c0c2b8
Sargis-Hovsepyan <sargishovsepyan@gmail.com.com>
2022-12-25 17:16:22 +0400

    [ADD] Added the getters, setter, and client action functions

--- includes/Client.hpp
+++ includes/Client.hpp
@@ -53,7 +53,42 @@ class Client
 
     public:
 
-        //TODO: Implement member functions
+        /* Costructor and Destructor */
+        
+        Client(int fd, int port, const std::string &hostname);
+        ~Client();
+
+        /* Getters */
+
+        int             get_fd() const;
+        int             get_port() const;
+
+        std::string     get_nickname() const;
+        std::string     get_username() const;
+        std::string     get_realname() const;
+        std::string     get_hostname() const;
+        std::string     get_prefix() const;
+
+    
+        /* Setters */
+
+        void            set_nickname(const std::string &nickname);
+        void            set_username(const std::string &username);
+        void            set_realname(const std::string &realname);
+        void            set_state(ClientState state);
+
+
+        /* Check state */
+
+        bool            is_registered() const;
+
+
+        /* Client actions */
+
+        void            welcome();
+
+        void            write(const std::string &message) const;
+        void            reply(const std::string &reply) const;
 };
 
 #endif
