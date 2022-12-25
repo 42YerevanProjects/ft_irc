ce1b5f16bcd4985e03179a8ac02d99704f71697b
Sargis-Hovsepyan <sargishovsepyan@gmail.com.com>
2022-12-25 16:41:53 +0400

    [ADD] Added the includes, enum ClientState and private member variables

--- includes/Client.hpp
+++ includes/Client.hpp
@@ -1,6 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Client.hpp	                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shovsepy <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/25 15:34:45 by shovsepy          #+#    #+#             */
+/*   Updated: 2022/12/25 16:36:09 by shovsepy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef IRC_CLIENT_HPP
 #define IRC_CLIENT_HPP
 
-class Client {};
+#include <sys/socket.h>
+#include <sys/poll.h>
+
+#include <string>
+#include <vector>
+
+#include "Channel.hpp"
+#include "response.hpp"
+
+
+enum ClientState
+{
+    HANDSHAKE,
+    LOGIN,
+    PLAY,
+    DISCONNECTED
+};
+
+
+class Client 
+{
+    typedef std::vector<pollfd>::iterator   pfd_iterator;
+
+    private:
+        
+        int             _fd;
+        int             _port;
+
+        std::string     _nickname;
+        std::string     _username;
+        std::string     _realname;
+        std::string     _hostname;
+
+        ClientState     _state;
+
+        Client();
+        Client(const Client &src);
+
+    public:
+
+        //TODO: Implement member functions
+};
 
 #endif
