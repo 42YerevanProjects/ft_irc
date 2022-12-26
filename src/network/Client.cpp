952cdc6f9eed5be86777a97cb0476017f7a1d2a2
Sargis-Hovsepyan <sargishovsepyan@gmail.com.com>
2022-12-26 16:23:27 +0400

    [FIX] Fixed some syntax errors

--- src/network/Client.cpp
+++ src/network/Client.cpp
@@ -14,8 +14,11 @@
 
 /* Constructor and Destructor */
 
-Client::Client(int, fd, int, port, const std::string &hostname)
-    : _fd(fd), _port(port), _hostname(hostname), _state(HANDSHAKE) {}
+Client::Client(int fd, int port, const std::string &hostname)
+    : _fd(fd), _port(port), _hostname(hostname), _state(HANDSHAKE), _channel(NULL) 
+{
+
+}
 
 
 Client::~Client() {}
@@ -24,7 +27,7 @@ Client::~Client() {}
 /* Getters */
 
 int             Client::get_fd() const { return _fd; }
-int             Client::get_port const { return _port; }
+int             Client::get_port() const { return _port; }
 
 std::string     Client::get_nickname() const { return _nickname; }
 std::string     Client::get_username() const { return _username; }
@@ -39,6 +42,8 @@ std::string     Client::get_prefix() const
     return _nickname + username + hostname;
 }
 
+Channel*        Client::get_channel() const { return _channel; }
+
 
 /* Setters */
 
@@ -46,9 +51,10 @@ void            Client::set_nickname(const std::string &nickname) { _nickname = 
 void            Client::set_username(const std::string &username) { _username = username; }
 void            Client::set_realname(const std::string &realname) { _realname = realname; }
 void            Client::set_state(ClientState state) { _state = state; }
+void            Client::set_channel(Channel *channel) { _channel = channel; }
 
 
 /* Check State */
 
-bool            Client::is_registered() const { return _state == ::REGISTERED; }
+bool            Client::is_registered() const { return _state == REGISTERED; }
 
