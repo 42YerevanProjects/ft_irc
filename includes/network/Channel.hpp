526eeb4746dfd41ec6770194c2cc3e3d4bf011c1
Sargis-Hovsepyan <sargishovsepyan@gmail.com.com>
2022-12-26 20:13:30 +0400

    [ADD] Added constructor and getter function declarations

--- includes/network/Channel.hpp
+++ includes/network/Channel.hpp
@@ -22,7 +22,41 @@ class Channel;
 
 class Channel 
 {
+    typedef std::vector<Client *>::iterator client_iterator;
+
+    private:
+
+        std::string             _name;
+        Client*                 _admin;
+        std::vector<Client *>   _clients;
+
+        /* Modes */
+        std::string             _k; // channel key
+        size_t                  _l; // limit of channel members
+        bool                    _n; // yes/no external messages
+
+        Channel();
+        Channel(const Channel& src);
     
+    public:
+
+        /* Constructor and Destructor */
+
+        Channel(const std::string &name, const std::string &key, Client* admin);
+        ~Channel();
+
+
+        /* Getters */
+
+        std::string                 get_name() const;
+        Client*                     get_admin() const;
+        
+        std::string                 get_key() const;
+        size_t                      get_limit() const;
+        bool                        ext_msg() const;
+
+        size_t                      get_size() const;
+        std::vector<std::string>    get_nicknames() const;
 };
 
 #endif
