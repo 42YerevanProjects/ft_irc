8e13549c1f8f9af1db517538eec5df2148593853
Sargis-Hovsepyan <sargishovsepyan@gmail.com.com>
2022-12-28 23:24:31 +0400

    [ADD] Added the command Quit implementation

---
+++ src/command/Quit.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Quit.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shovsepy <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/28 23:19:10 by shovsepy          #+#    #+#             */
+/*   Updated: 2022/12/28 23:24:45 by shovsepy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "command/Command.hpp"
+
+Quit::Quit(Server* srv) : Command(srv) {}
+
+Quit::~Quit() {}
+
+// syntax: QUIT [<message>]
+
+void    Quit::execute(Client* client, std::vector<std::string> args)
+{
+    std::string reason = args.empty() ? "Leaving..." : args.at(0);
+
+    if (reason.at(0) == ':')
+        reason = reason.substr(1);
+
+    client->write(RPL_QUIT(client->get_prefix(), reason));
+	_srv->on_client_disconnect(client->get_fd());
+}
