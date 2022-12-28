702585f045e9771c4f5302197a98c39b3141d1a6
Sargis-Hovsepyan <sargishovsepyan@gmail.com.com>
2022-12-29 03:49:25 +0400

    [ADD] The User command class implementation

---
+++ src/command/User.cpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   User.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shovsepy <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/29 03:19:09 by shovsepy          #+#    #+#             */
+/*   Updated: 2022/12/28 03:30:29 by shovsepy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "command/Command.hpp"
+
+User::User(Server* srv) : Command(srv) {}
+
+User::~User() {}
+
+// stntax: USER <username> <hostname> <servername> <realname>
+
+void    User::execute(Client* client, std::vector<std::string> args)
+{
+    if (client->is_registered())
+    {
+        client->reply(ERR_ALREADYREGISTERED(client->get_nickname()));
+        return;
+    }
+
+    if (args.size() < 4) 
+    {
+        client->reply(ERR_NEEDMOREPARAMS(client->get_nickname(), "USER"));
+        return;
+    }
+
+    client->set_username(args[0]);
+    client->set_realname(args[3]);
+    client->welcome();
+}
