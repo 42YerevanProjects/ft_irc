c077971f2bc68767f95406025eb3044aa64e697d
Sargis-Hovsepyan <sargishovsepyan@gmail.com.com>
2022-12-30 16:41:26 +0400

    [UPDATE] Changed the log message color

--- includes/response.hpp
+++ includes/response.hpp
@@ -66,18 +66,18 @@
 
 static inline void log(const std::string& message) 
 {
-	time_t      rawtime;
-	struct tm   *timeinfo;
-	char        buffer[80];
+    time_t      rawtime;
+    struct tm   *timeinfo;
+    char        buffer[80];
 
-	time(&rawtime);
-	timeinfo = localtime(&rawtime);
+    time(&rawtime);
+    timeinfo = localtime(&rawtime);
 
-	strftime(buffer, sizeof(buffer), "%d-%m-%Y %H:%M:%S", timeinfo);
-	std::string str(buffer);
+    strftime(buffer, sizeof(buffer), "%d-%m-%Y %H:%M:%S", timeinfo);
+    std::string str(buffer);
 
-	(void)message;
-	std::cout << "\033[0;34m[" << str << "]\033[0m ";
+    (void)message;
+    std::cout << "\033[0;34m[" << str << "]\033[0m ";
     std::cout << message << std::endl;
 }
 
